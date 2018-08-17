f5c0775ad7b66450b46ccca7f13a0c6d23023d44
Dag Erik <dagerikhl@gmail.com>
2018-08-17 13:18:34 +0200

    Fix unresolved pictures in res folder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default () => <header>
     <div className="img-circle-cropper">
-        <img src="res/dog.jpg" alt="The Holy Animals" title="The Holy Animals"/>
+        <img src={'res/dog.jpg'} alt="The Holy Animals" title="The Holy Animals"/>
     </div>
     <h1>Awesome Holy Animals</h1>
     <div className="img-circle-cropper">
-        <img src="res/me.png" alt="Me" title="Me"/>
+        <img src={'res/me.png'} alt="Me" title="Me"/>
     </div>
 </header>;
