90988d1ed45017581bece7dcb5a0647c6a8c08ac
Dag Erik <dagerikhl@gmail.com>
2018-08-17 11:14:47 +0200

    Fix error with path in webpack and add start script

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path(__dirname, 'src/index.jsx'),
+    entry: path.resolve(__dirname, 'src/index.jsx'),
     mode: 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     plugins: [
-        new HtmlWebpackPlugin({ template: path(__dirname, 'index.html') })
+        new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'index.html') })
     ],
     module: {
         rules: [
